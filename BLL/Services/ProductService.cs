8169af6370cb71dc03c61784caf77ba61a92e193
MSI <MSI@DESKTOP-T27O90D>
2025-09-10 22:30:40 +0700

    Add project files.

---
+++ BLL/Services/ProductService.cs
@@ -0,0 +1,13 @@
+﻿using BLL.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    internal class ProductService : IProductService 
+    {
+    }
+}
