32d3adb85240097f42ba647f003b599c5ecfbba6
luanlee <ltluana23109@cusc.ctu.edu.vn>
2025-09-30 22:42:00 +0700

    UI Admin

---
+++ ASP_LorKingDom/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_LorKingDom.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View(); // tìm Views/Admin/Dashboard.cshtml
+        }
+    }
+}
