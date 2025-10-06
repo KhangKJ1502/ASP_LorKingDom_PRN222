28603e48a0a298f54aae8ba6bc4fe9d207e8e8b3
luanlee08 <luanlevn2@gmail.com>
2025-10-06 11:26:17 +0700

    Luan Dev frontend Contact

--- ASP_LorKingDom/Controllers/HomeController.cs
+++ ASP_LorKingDom/Controllers/HomeController.cs
@@ -22,6 +22,15 @@ namespace ASP_LorKingDom.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
